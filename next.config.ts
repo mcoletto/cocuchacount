71813fed595c1f8263f3eebb02a228958c95ada4
Martina Coletto <89276607+mcoletto@users.noreply.github.com>
2026-04-04 22:22:06 -0300

    fix: deshabilitar router cache de Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000"],
+    staleTimes: {
+      dynamic: 0,
+      static: 0,
     },
   },
 };
